c297fee7a54147366a4d749f0f9bb96fd03e16ed
lgz920 <lgz_920@163.com>
2019-04-01 16:22:53 +0800

    zxing优化，删除与二维码扫描无关的类

--- zxing/src/main/java/com/google/zxing/client/android/ScanFromWebPageManager.java
+++ zxing/src/main/java/com/google/zxing/client/android/ScanFromWebPageManager.java
@@ -18,7 +18,6 @@ package com.google.zxing.client.android;
 
 import android.net.Uri;
 import com.google.zxing.Result;
-import com.google.zxing.client.android.result.ResultHandler;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -52,16 +51,16 @@ final class ScanFromWebPageManager {
     return returnUrlTemplate != null;
   }
 
-  String buildReplyURL(Result rawResult, ResultHandler resultHandler) {
-    String result = returnUrlTemplate;
-    result = replace(CODE_PLACEHOLDER,
-                     returnRaw ? rawResult.getText() : resultHandler.getDisplayContents(), result);
-    result = replace(RAW_CODE_PLACEHOLDER, rawResult.getText(), result);
-    result = replace(FORMAT_PLACEHOLDER, rawResult.getBarcodeFormat().toString(), result);
-    result = replace(TYPE_PLACEHOLDER, resultHandler.getType().toString(), result);
-    result = replace(META_PLACEHOLDER, String.valueOf(rawResult.getResultMetadata()), result);
-    return result;
-  }
+//  String buildReplyURL(Result rawResult, ResultHandler resultHandler) {
+//    String result = returnUrlTemplate;
+//    result = replace(CODE_PLACEHOLDER,
+//                     returnRaw ? rawResult.getText() : resultHandler.getDisplayContents(), result);
+//    result = replace(RAW_CODE_PLACEHOLDER, rawResult.getText(), result);
+//    result = replace(FORMAT_PLACEHOLDER, rawResult.getBarcodeFormat().toString(), result);
+//    result = replace(TYPE_PLACEHOLDER, resultHandler.getType().toString(), result);
+//    result = replace(META_PLACEHOLDER, String.valueOf(rawResult.getResultMetadata()), result);
+//    return result;
+//  }
 
   private static String replace(CharSequence placeholder, CharSequence with, String pattern) {
     CharSequence escapedWith = with == null ? "" : with;
