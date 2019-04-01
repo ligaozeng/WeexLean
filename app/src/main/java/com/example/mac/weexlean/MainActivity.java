f4ec2d7b005a319ca69913cc6bd3072e11e68d4f
lgz920 <lgz_920@163.com>
2019-04-01 16:34:11 +0800

    扫码功能跑通

--- app/src/main/java/com/example/mac/weexlean/MainActivity.java
+++ app/src/main/java/com/example/mac/weexlean/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.mac.weexlean;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.client.android.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,8 +30,21 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this,CaptureActivity.class);
+                startActivityForResult(intent,1001);
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1001 && resultCode== Activity.RESULT_OK)
+        {
+            String result=data.getStringExtra(CaptureActivity.KEY_DATA);
+            Toast.makeText(this, "qrcode result is "+result, Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
